f20f61a316034c98b2dc4cb2e5c1b9fb6b2d391a
AlysonAtwell <97715679+AlysonAtwell@users.noreply.github.com>
2022-10-07 22:49:02 -0400

    Update Random generator.py

--- Random generator.py
+++ Random generator.py
@@ -3,12 +3,30 @@ from random import randint
 from turtle import right
 
 
+
 root = Tk() # initialize tkinter 
 root.title( "Random workout")# Title of Application
 root.geometry("925x500+300+200")#Set the geometry of frame
 #ICONPHOTO
+# root.iconbitmap("C:\zebra\photos for gui\fun for all ages")
 root.configure(bg="#E4F7FF")
 root.resizable(False,False) # can not be resizing window
+
+y= IntVar() # initialize y value which are the picks for the radiobutton to link to button in new window pick button   
+    
+def warm():
+    if (Y.get()==0):
+        return pick1
+    elif (Y.get()==1):
+        return pick2
+    elif (Y.get()==2):
+        return pick3
+    else:
+        print('Pick a position to do warm-ups') # maybe place button 
+
+variable=y, # groups pick 1, pick2, pick 3 together if they share the same v
+
+
 
 new_window =""
 def openwindow():
@@ -21,42 +39,67 @@ def openwindow():
     lbl = Label(new_window, text="Congratulations, here's your workout!",font=("Helvetica",30),bg="#E4F7FF", fg= "blue",)
     lbl.pack(pady=18)
     btn2 =Button(new_window, text="Close Me",pady=7,padx=20, bg="#E4F7FF", fg= "blue", border=0, command=lambda: new_window.destroy()).pack(anchor=E) # Button to New warm up window
-    winButton = Button(new_window, text="Start Your Warm UP!!", font=("Helvetica",24),fg= "blue", bg="#E4F7FF", command = pick)
-    winButton.pack()
-
-
-
+    pickButton = Button(new_window, text="Start Your Warm UP!!", font=("Helvetica",24),fg= "blue", bg="#E4F7FF", command = warm)
+    pickButton.pack()
+    
+    #need to fix command to pick and display only that one list
+    
+    
+        
 
 #having trouble installing images #Radio buttons to choose workout
-#standimage = PhotoImage(file="E:\Gym-bor-e_tkinter_project\photos for gui\chair.png") #
+#chairimage = PhotoImage(file=   )
+#standimage = PhotoImage(file=E:\Gym-bor-e_tkinter_project\photos for gui") #
 #floorimage = PhotoImage(file="E:\Gym-bor-e_tkinter_project\photos for gui\exercises.png") #
 #positionimages= ["chairimage","standimage',"floorimage"] # Position for exercise
 
-chair=["Neck Stretch","Knee Lifts","Sit and Stand","Heel Slides","Tummy Twists for Abs","Seated Jumping Jacks","Seated Forward Bend","Knee to Chest","Ankle Rotations","Bicep curls with Resistance Bands"]
-stand=["Knee Lifts","Sit and Stand","Squats","Arm Circles: to release tension in your shoulders","Overhead arm reaches: the best exercise to kickstart energy and muscle warmth","Hip Rotations: ideal for loosening your lower body","Side knee lifts: the best exercise for strengthening your core","Squat with arm lift: for an entire body burn","Squat with reaches: to fire up your glutes","Lunges: to warmup your thighs","Jumping Jacks: to increase blood flow and loosen your entire body","Plank Walk Outs: to engage your core, hamstrings, and shoulders","High Knees: get your blood pumping and activates the muscles","Jumping Rope: maximize your heart rate","Knee Lifts", "Stand and Sit", "squat"]
-floor=["Knee to Chest","dead bug","Heel Touch", "Toe Taps","Overhead arm reaches: the best exercise to kickstart energy and muscle warmth","Push-ups: for an all-over arm burn","Snow Angels: to increase blood flow and loosen your entire body","Plank Walk Outs: to engage your core, hamstrings, and shoulders","High Knees: get your blood pumping and activates the muscles","Hug knees to chest"]
-
+x= IntVar()
+    
 def order():
     if (x.get()==0):
-        chair.get()
+        return pick1
     elif (x.get()==1):
-        stand.get() 
+        return pick2
     elif (x.get()==2):
-        floor.get()
+        return pick3
     else:
-        print('Pick a location to do Warm-ups') # maybe place button
-        
+        print('Pick a position to do warm-ups') # maybe place button  
+       
+     
+       
 
-def pick():
-    warmups = ["Heel Touch", "Squats", "Toe Taps","Arm Circles: to release tension in your shoulders","Overhead arm reaches: the best exercise to kickstart energy and muscle warmth","Hip Rotations: ideal for loosening your lower body","Side knee lifts: the best exercise for strengthening your core","Squat with arm lift: for an entire body burn","Squat with reaches: to fire up your glutes","Lunges: to warmup your thighs","Push-ups: for an all-over arm burn","Jumping Jacks: to increase blood flow and loosen your entire body","Plank Walk Outs: to engage your core, hamstrings, and shoulders","High Knees: get your blood pumping and activates the muscles","Jumping Rope: maximize your heart rate"]
+def pick1():
+    warmups = ["Neck Stretch","Knee Lifts","Sit and Stand","Heel Slides","Tummy Twists for Abs","Seated Jumping Jacks","Seated Forward Bend","Knee to Chest","Ankle Rotations","Bicep curls with Resistance Bands"]
     warmups_set = set(warmups)#convert to a set
     unique_warmups= list(warmups_set)#Convert back to list
     our_number = len(unique_warmups) - 1 # create our list size variable 
     rando = randint(0, our_number ) # create a random number between 0 and 5 
     #winnerLabel= Label(root,text=len(entries), font=("Helvetica", 18)) previously needed to get the winner
-    winnerLabel= Label(new_window,text=unique_warmups[rando], font=("Comic Sans MS", 18),fg ="blue", bg="#E4F7FF")
-    winnerLabel.pack(pady=10)
+    pick1_Label= Label(new_window,text=unique_warmups[rando], font=("Comic Sans MS", 18),fg ="blue", bg="#E4F7FF")
+    pick1_Label.pack(pady=10)
+
+def pick2():
+    warmups = ["Knee Lifts","Sit and Stand","Squats","Arm Circles: to release tension in your shoulders","Overhead arm reaches: the best exercise to kickstart energy and muscle warmth","Hip Rotations: ideal for loosening your lower body","Side knee lifts: the best exercise for strengthening your core","Squat with arm lift: for an entire body burn","Squat with reaches: to fire up your glutes","Lunges: to warmup your thighs","Jumping Jacks: to increase blood flow and loosen your entire body","Plank Walk Outs: to engage your core, hamstrings, and shoulders","High Knees: get your blood pumping and activates the muscles","Jumping Rope: maximize your heart rate","Knee Lifts", "Stand and Sit", "squat"]
+    warmups_set = set(warmups)#convert to a set
+    unique_warmups= list(warmups_set)#Convert back to list
+    our_number = len(unique_warmups) - 1 # create our list size variable 
+    rando = randint(0, our_number ) # create a random number between 0 and 5 
+    #winnerLabel= Label(root,text=len(entries), font=("Helvetica", 18)) previously needed to get the winner
+    pick2Label= Label(new_window,text=unique_warmups[rando], font=("Comic Sans MS", 18),fg ="blue", bg="#E4F7FF")
+    pick2Label.pack(pady=10)
+
+def pick3():
+    warmups = ["Knee to Chest","dead bug","Heel Touch", "Toe Taps","Overhead arm reaches: the best exercise to kickstart energy and muscle warmth","Push-ups: for an all-over arm burn","Snow Angels: to increase blood flow and loosen your entire body","Plank Walk Outs: to engage your core, hamstrings, and shoulders","High Knees: get your blood pumping and activates the muscles","Hug knees to chest"]
+    warmups_set = set(warmups)#convert to a set
+    unique_warmups= list(warmups_set)#Convert back to list
+    our_number = len(unique_warmups) - 1 # create our list size variable 
+    rando = randint(0, our_number ) # create a random number between 0 and 5 
+    #winnerLabel= Label(root,text=len(entries), font=("Helvetica", 18)) previously needed to get the winner
+    pick3Label= Label(new_window,text=unique_warmups[rando], font=("Comic Sans MS", 18),fg ="blue", bg="#E4F7FF")
+    pick3Label.pack(pady=10)
     
+       
+
 welLabel = Label(root, text="Welcome!", fg ="blue", bg="#E4F7FF", font=("Helvetica",30)) #Welcom Label Header label 
 welLabel.pack(pady=20)
 
@@ -67,9 +110,13 @@ topLabel = Label(root, text="Now Pick Location", fg ="blue", bg="#E4F7FF", font=
 topLabel.pack(pady=10)
 
 
-x= IntVar()
+def new_func():
+    x= IntVar()
+    return x
 
-choose_workout = ['chair', 'stand', 'floor']
+x = new_func()
+
+choose_workout = ['Chair', 'Stand', 'Floor']
 
 for index in range(len(choose_workout)):
     radiobutton = Radiobutton(root, 
@@ -87,14 +134,10 @@ for index in range(len(choose_workout)):
                                 )
     radiobutton.pack()
 
-
 btn=Button(root, text="Go to Workout",font = ("Helvetica",25), width=39, pady=7, bg="#E4F7FF", fg= "blue", border=0, command = openwindow) # was open window not go to workout
 btn.pack(padx=20, pady=20)
-
-
 
 Button(root,text="Close Warm UP Window",pady=7,padx=20, bg="#E4F7FF", fg= "blue", border=0, command=lambda: new_window.destroy()).pack(anchor=E) # Button to New warm up window
 
 
-
-root.mainloop()
+root.mainloop() # must have to display window  
